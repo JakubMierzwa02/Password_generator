4664d0d08111f1fb94ed6e4144987089944916bd
Jakub Mierzwa <kontakt.jakubmierzwa@gmail.com>
2023-11-11 18:52:00 +0100

    Tests added

--- test/password_generator_tests.cpp
+++ test/password_generator_tests.cpp
@@ -1,7 +1,53 @@
 #include <gtest/gtest.h>
 #include "..\password_generator.hpp"
 
-TEST(PasswordGeneratorTest, BasicTest)
+// Password length test
+TEST(PasswordGeneratorTest, LengthTest)
 {
-	ASSERT_TRUE(true);
+	int length = 10;
+	std::string password = generatePassword(length, true, true);
+	ASSERT_EQ(length, password.size());
+}
+
+TEST(PasswordGeneratorTest, ContentTest)
+{
+	std::string password = generatePassword(10, true, true);
+
+	bool hasNumber = false;
+	bool hasSpecialChar = false;
+	for (char c : password)
+	{
+		if (isdigit(c))
+			hasNumber = true;
+		if (ispunct(c))
+			hasSpecialChar = true;
+	}
+
+	ASSERT_TRUE(hasNumber);
+	ASSERT_TRUE(hasSpecialChar);
+}
+
+// Randomness test
+TEST(PasswordGeneratorTest, RandomnessTest) 
+{
+    std::string password1 = generatePassword(10, true, true);
+    std::string password2 = generatePassword(10, true, true);
+    ASSERT_NE(password1, password2);
+}
+
+TEST(PasswordGeneratorTest, UniquenessTest)
+{
+	std::set<std::string> generated_passwords;
+	for (int i = 0; i < 1000; i++)
+	{
+		std::string password = generatePassword(10, true, true);
+		ASSERT_TRUE(generated_passwords.find(password) == generated_passwords.end());
+		generated_passwords.insert(password);
+	}
+}
+
+int main(int argc, char **argv) 
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
